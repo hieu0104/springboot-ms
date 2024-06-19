97c60ddee53c5e0d0b6852065465cdc06e76fcb4
hieu0104 <lechihieubl2020@gmail.com>
2024-06-19 16:06:53 +0700

    Updated Pom.xml

--- src/main/java/com/hieu/ms/MSApplication.java
+++ src/main/java/com/hieu/ms/MSApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MSApplication {
     public static void main(String[] args) {
+
         SpringApplication.run(MSApplication.class, args);
     }
 }
